0f4f3224f9aa9fc8f363c2c80e8558f37a100de8
Aditya <adityang5@gmail.com>
2020-05-09 16:05:21 +0530

    Distance Sensor

---
+++ scanner_fast/scanner_fast.ino
@@ -0,0 +1,92 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/
+
+#include <Servo.h>
+const int pingPin = 7; // Trigger Pin of Ultrasonic Sensor
+const int echoPin = 6; // Echo Pin of Ultrasonic Sensor
+
+Servo myservo, myservo1;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int theta = 0, phi = 60, f1=20, f=20;    // variable to store the servo phiition
+//int lb=100,ub=160, lb1=60, ub1=160;
+
+int lb1=70,ub1=180, lb=100, ub=120;
+
+//int lb=100,ub=160, lb1=109, ub1=111;
+
+void setup() {
+  myservo1.attach(10);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(9);
+  Serial.begin(115200); // Starting Serial Terminal
+  phi = lb, theta = lb1;
+}
+
+
+void loop() {
+  //myservo.write(180);
+  //myservo1.write(120);
+  //return;
+  long duration, inches, r;
+   pinMode(pingPin, OUTPUT);
+   digitalWrite(pingPin, LOW);
+   delayMicroseconds(2);
+   digitalWrite(pingPin, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(pingPin, LOW);
+   pinMode(echoPin, INPUT);
+   duration = pulseIn(echoPin, HIGH);
+   inches = microsecondsToInches(duration);
+   r = microsecondsToCentimeters(duration);
+   //Serial.print(inches);
+   //Serial.print("in, ");
+   if (r<50) {
+     Serial.print(r);
+     Serial.print(" ");
+     Serial.print(theta);
+     Serial.print(" ");
+     Serial.print(phi);
+     Serial.print(" ");
+     Serial.println();
+   }
+  theta += f1;
+  if (!(lb1<=theta && theta<=ub1)) {
+    if (theta>=ub1) {
+      f1 = -1;
+    } else if (theta<=lb1) {
+      f1 = 1;
+    } else {
+      theta = lb1;
+      f1 = 1;
+    }
+
+    phi += f;
+    if (!(lb<=phi && phi<=ub)) {
+      if (phi>=ub) {
+        f = -1;
+      } else if (phi<=lb) {
+        f = 1;
+      } else {
+        phi = lb;
+        f = 1;
+      }
+    }
+  }
+  myservo.write(phi);
+  myservo1.write(theta);
+  delay(100);
+}
+
+long microsecondsToInches(long microseconds) {
+   return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds) {
+   return microseconds / 29 / 2;
+}
---
+++ web/distance_sensor.py
@@ -0,0 +1,54 @@
+###
+# Plotter Code
+###
+#
+import numpy as np
+import sys
+
+# Serial Comms
+import random
+
+import serial
+from math import sin, cos, pi, atan
+
+def start_scan():
+    input("Enter to start")
+    print("Connecting to serial")
+    s = serial.Serial("/dev/cu.usbserial-1410", 115200)
+    while True:
+        try:
+            r, theta, phi = list(map(int, s.readline().split()))
+            #r, phi, theta = list(map(int, s.readline().split()))
+            print("Analysing", [r, theta, phi])
+
+            r = r + (random.uniform(-0.3, 0.3))
+            phi = phi - 100
+            theta = theta + 90
+
+            theta = theta * pi/180
+            phi = phi * pi/180
+
+            #r = r/3.125 # Scaling 
+            #r = r*0.8 # Scaling 
+
+            sf = 1
+            d = 2.7 * sf
+            R = 1.2 * sf
+
+            phi_d = atan( ( (r+R)*sin(phi) + d*cos(phi) ) / ( (r+R)*cos(phi) - d*sin(phi) ) )
+            r_d = ( (r+R)*cos(phi) - d*sin(phi) )/cos(phi)
+
+            #r = r_d
+            #phi = phi_d
+
+            dist = r*cos(phi)
+            for r_i in r:
+                dist = r_i*cos(phi)
+                if dist < 5: # Distance in cm
+                    print("Too close ", {"r": r_i, "dist", dist})
+                
+        except Exception as e:
+            print(e)
+
+
+start_scan()
