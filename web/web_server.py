2887ebc778a3303091e36af001a0ce378487e8c5
Aditya <adityang5@gmail.com>
2020-05-11 17:21:09 +0530

    New Car Migration

--- web/web_server.py
+++ web/web_server.py
@@ -131,7 +131,7 @@ with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
         dir(camera)
         output = StreamingOutput()
         #Uncomment the next line to change your Pi's Camera rotation (in degrees)
-        #camera.rotation = 90
+        camera.rotation = 180
         camera.start_recording(output, format='mjpeg')
         print("Camera opened, starting server at port 8080")
         try:
