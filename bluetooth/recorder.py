207530913f4106cd16e220077e6308197d572652
Aditya <adityang5@gmail.com>
2020-06-12 13:20:49 +0530

    Bluetooth Contoller

--- bluetooth/recorder.py
+++ bluetooth/recorder.py
@@ -94,13 +94,13 @@ import time
 import cv2
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
-camera.resolution = '640x480' #(640, 480)
+camera.resolution = (640, 480)
 camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
 # allow the camera to warmup
 time.sleep(0.1)
 # capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+for frame in camera.capture_continuous(stream, format='jpeg'): # camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 	# grab the raw NumPy array representing the image, then initialize the timestamp
 	# and occupied/unoccupied text
 	image = frame.array
