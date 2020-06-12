ffb0bc1d25297fec1cf2512d082cbc663eb19e51
Aditya <adityang5@gmail.com>
2020-06-12 15:13:40 +0530

    Bluetooth Contoller

--- bluetooth/compile.py
+++ bluetooth/compile.py
@@ -5,11 +5,13 @@ def compile_training_data():
         print('Compiling Training Data')
         td = os.path.join(os.getcwd(), 'training_data')
         folders = [ name for name in os.listdir(td) if os.path.isdir(os.path.join(td, name)) ]
+        files = [ name for name in os.listdir(td) if str(name).endswith(".zip") ]
         for i in folders:
-                print("\t Compiling : ", i)
-                shutil.copyfile("player.py", os.path.join(os.getcwd(), 'training_data', i, "player.py"))
-                c_dir = os.path.join(os.getcwd(), 'training_data', i)
-                shutil.make_archive(c_dir, 'zip', c_dir)
+                if i+".zip" not in files:
+                        print("\t Compiling : ", i)
+                        shutil.copyfile("player.py", os.path.join(os.getcwd(), 'training_data', i, "player.py"))
+                        c_dir = os.path.join(os.getcwd(), 'training_data', i)
+                        shutil.make_archive(c_dir, 'zip', c_dir)
         print("Done Compiling data")
 
 
