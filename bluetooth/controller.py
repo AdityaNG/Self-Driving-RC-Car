2e24f54d338d3a5558db17ca38d20bfccd059c4c
Aditya <adityang5@gmail.com>
2020-06-12 21:45:13 +0530

    Shutdown signal

--- bluetooth/controller.py
+++ bluetooth/controller.py
@@ -262,9 +262,8 @@ while True:
                         LED_PATTERN("R_")
                         time.sleep(0.5)
                         #os.system("halt")
-            
-            if event.code == 17 and event.value==0 and event.type==3:
-                if now - shutdown_request >= 2 and shutdown_request!=0:    
+            else:
+                if now - shutdown_request < 2 and shutdown_request!=0:    
                     print("Shutdown request dropped")
                     LED_PATTERN("G_")
                     shutdown_request = 0
