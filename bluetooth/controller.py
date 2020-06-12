a1b96672e9e1dae39b95feefd631a42824bb01b1
Aditya <adityang5@gmail.com>
2020-06-12 21:52:56 +0530

    Shutdown signal

--- bluetooth/controller.py
+++ bluetooth/controller.py
@@ -236,12 +236,12 @@ while True:
         SPEED_MODE = 1
 
         now = time.time()
-            if shutdown_request!=0:
-                if now - shutdown_request >= 2: # Shutdown button pressed for 2 seconds or more
-                    print("SHUTDOWN SIGNAL")
-                    LED_PATTERN("R_")
-                    time.sleep(0.5)
-                    #os.system("halt")
+        if shutdown_request!=0:
+            if now - shutdown_request >= 2: # Shutdown button pressed for 2 seconds or more
+                print("SHUTDOWN SIGNAL")
+                LED_PATTERN("R_")
+                time.sleep(0.5)
+                #os.system("halt")
 
         for event in gamepad.read_loop():
             #accel_val = 0
