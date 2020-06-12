2e27840e70a4d458089d11f1cdfa1193997b9775
Aditya <adityang5@gmail.com>
2020-06-12 13:08:25 +0530

    Bluetooth Contoller

--- bluetooth/recorder.py
+++ bluetooth/recorder.py
@@ -94,7 +94,7 @@ import time
 import cv2
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
-camera.resolution = (640, 480)
+camera.resolution = '640x480' #(640, 480)
 camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
 # allow the camera to warmup
