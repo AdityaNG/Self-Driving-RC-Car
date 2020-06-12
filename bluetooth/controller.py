55b2e16a364aca41a2c014ebea2616be4abcc952
Aditya <adityang5@gmail.com>
2020-06-12 14:18:43 +0530

    Bluetooth Contoller

--- bluetooth/controller.py
+++ bluetooth/controller.py
@@ -141,8 +141,11 @@ def loop(accel_val, steering_angle, rec_toggle=False):
     #print("accel_val", accel_val)
     #print("steering_angle", steering_angle)
 
-    prefs.set_pref("accel_val", accel_val)
-    prefs.set_pref("steering_angle", steering_angle)
+    if (accel_val!=''):
+        prefs.set_pref("accel_val", accel_val)
+    
+    if (steering_angle!=''):
+        prefs.set_pref("steering_angle", steering_angle)
 
     #rec = prefs.get_pref("rec")
     #accel_val = int(prefs.get_pref("accel_val"))
