0b6fc0883e7ae548eff28142a659049384413b95
Aditya <adityang5@gmail.com>
2020-06-13 22:41:01 +0530

    Autopilot testing

--- bluetooth/autopilot_web_server.py
+++ bluetooth/autopilot_web_server.py
@@ -1,107 +1,40 @@
-import io
-try:
-    import picamera
-except:
-    print("ModuleNotFoundError: No module named 'picamera'")
-import logging
-import socketserver
-from threading import Condition
-from http import server
-from os import curdir, sep
+from flask import Flask, render_template, Response, request
+# Raspberry Pi camera module (requires picamera package, developed by Miguel Grinberg)
+from camera_pi import Camera
 from urllib.parse import parse_qs
 import time
-
 import prefs
 
-#f = open('index.html')
-#PAGE=f.read()
+app = Flask(__name__)
 
+@app.route('/?')
+def index():
+        """Video streaming home page."""
+        PAGE = "{'status': 'ok'}"
+        try:
+                params = dict()
+                params["accel_val_auto"] = request.args.get('accel_val_auto')
+                params["steering_angle_auto"] = request.args.get('steering_angle_auto')
+                for d in params:
+                        prefs.set_pref(d, params[d][0])
+                        prefs.set_pref("last_message", str(time.time()))
+                        #print("GOT : ", params)
+        except Exception as e:
+                PAGE = "{'status': 'not ok', 'error': '" + str(e) + "' }"
+        return PAGE
 
-class StreamingOutput(object):
-        def __init__(self):
-                self.frame = None
-                self.buffer = io.BytesIO()
-                self.condition = Condition()
+def gen(camera):
+        """Video streaming generator function."""
+        while True:
+                frame = camera.get_frame()
+                yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-        def write(self, buf):
-                if buf.startswith(b'\xff\xd8'):
-                        # New frame, copy the existing buffer's content and notify all
-                        # clients it's available
-                        self.buffer.truncate()
-                        with self.condition:
-                                self.frame = self.buffer.getvalue()
-                                self.condition.notify_all()
-                        self.buffer.seek(0)
-                return self.buffer.write(buf)
+@app.route('/stream.mjpg')
+def video_feed():
+        """Video streaming route. Put this in the src attribute of an img tag."""
+        return Response(gen(Camera()),
+                        mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
-class StreamingHandler(server.BaseHTTPRequestHandler):
-        def do_GET(self):
-                global params
-                if "/?" in self.path:
-                        
-                        PAGE = "{'status': 'ok'}"
-                        try:
-                                params = parse_qs(self.path[2:])
-                                for d in params:
-                                    prefs.set_pref(d, params[d][0])
-                                prefs.set_pref("last_message", str(time.time()))
-                                #print("GOT : ", params)
-                        except Exception as e:
-                                PAGE = "{'status': 'not ok', 'error': '" + str(e) + "' }"
-                        self.send_response(200)
-                        self.send_header('Content-Type', 'text/json')
-                        content = PAGE.encode('utf-8')
-                        self.send_header('Content-Length', len(content))
-                        self.end_headers()
-                        self.wfile.write(content)
-                
-                elif self.path == '/stream.mjpg':
-                        self.send_response(200)
-                        self.send_header('Age', 0)
-                        self.send_header('Cache-Control', 'no-cache, private')
-                        self.send_header('Pragma', 'no-cache')
-                        self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-                        self.end_headers()
-                        try:
-                                while True:
-                                        with output.condition:
-                                                output.condition.wait()
-                                                frame = output.frame
-                                        self.wfile.write(b'--FRAME\r\n')
-                                        self.send_header('Content-Type', 'image/jpeg')
-                                        self.send_header('Content-Length', len(frame))
-                                        self.end_headers()
-                                        self.wfile.write(frame)
-                                        self.wfile.write(b'\r\n')
-                        except Exception as e:
-                                logging.warning(
-                                        'Removed streaming client %s: %s',
-                                        self.client_address, str(e))
-                else:
-                        self.send_error(404)
-                        self.end_headers()
-
-class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
-        allow_reuse_address = True
-        daemon_threads = True
-
-try:
-    with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
-            dir(camera)
-            output = StreamingOutput()
-            #Uncomment the next line to change your Pi's Camera rotation (in degrees)
-            camera.rotation = 180
-            camera.start_recording(output, format='mjpeg')
-            print("Camera opened, starting server at port 8080")
-except:
-    print("Camera Error")
-finally:
-
-    try:
-        print("Starting Server")
-        address = ('', 8080)
-        server = StreamingServer(address, StreamingHandler)
-        server.serve_forever()
-    finally:
-        camera.stop_recording()
+if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=8080, debug=False, threaded=True)
