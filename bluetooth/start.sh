105249e8d0f69569fc71cf032c35b80b132e44bf
Aditya <adityang5@gmail.com>
2020-06-13 22:02:23 +0530

    Autopilot testing

--- bluetooth/start.sh
+++ bluetooth/start.sh
@@ -1,4 +1,4 @@
-sudo modprobe bcm2835-v4l2
+# sudo modprobe bcm2835-v4l2
 
 python3 controller.py &
 python3 autopilot_web_server.py &
--- bluetooth/start_back.sh
+++ bluetooth/start_back.sh
@@ -1,4 +1,4 @@
-sudo modprobe bcm2835-v4l2
+# sudo modprobe bcm2835-v4l2
 
 python3 controller.py   > logs/controller.txt &
 python3 autopilot_web_server.py > logs/autopilot_web_server.txt &
