c3ded9d39bcd643e1106263c99d5ef9cb7daf34b
Aditya <adityang5@gmail.com>
2020-07-21 12:27:46 +0530

    Wheel Speed

--- bluetooth/controller.py
+++ bluetooth/controller.py
@@ -340,12 +340,13 @@ def decodeImage(image_bytes):
 wheel_speed_counter = 0
 wheel_speed_counter_last_set = time.time()
 wheel_speed_delay = 1 # Calculate every 1 seconds
+gear_ratio = 0.769230769
 def speed_calculator():
     global wheel_speed_counter, wheel_speed_counter_last_set, wheel_speed_delay
     time.sleep(10) 
     while True:
         try:
-
+            now = time.time()
             reading = GPIO.input(wheel_speed_data_pin)  
             if reading:
                 wheel_speed_counter += 1
@@ -353,17 +354,19 @@ def speed_calculator():
                     time.sleep(0.01)
                     pass # Wait for the sensor to read 0 again before reading the next 1
             
-            now = time.time()
+            
             if abs(now - wheel_speed_counter_last_set)>=wheel_speed_delay:
-                log("speed_calculator - counts", wheel_speed_counter)
                 wheel_speed_counter_last_set = now
                 wheel_speed_counter = 0
 
-            #log("speed_calculator", reading)
+                #log("speed_calculator", reading)
 
-            #speed = float(prefs.get_pref("speed"))
-            #accel_val = float(prefs.get_pref("accel_val"))
-            #speed = chase_value(accel_val, speed, 0.5)
+                speed = float(prefs.get_pref("speed"))
+                #accel_val = float(prefs.get_pref("accel_val"))
+                speed = chase_value(wheel_speed_counter * gear_ratio, speed, 0.5)
+                prefs.set_pref("speed", abs(speed), wheel_speed_counter)
+                
+                log("speed_calculator", speed)
 
             #global Camera
             #frame = decodeImage(Camera().get_frame())
